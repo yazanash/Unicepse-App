409c1b7fa2619e63ed3ba9b4bdb8e5e82d9580b1
yazanash <yazan.ash.doonaas@gmail.com>
2023-11-08 17:27:54 +0200

    PG-16 create player service #PG-16 To Verify

--- PlatinumGym.Core/Services/IDataService.cs
+++ PlatinumGym.Core/Services/IDataService.cs
@@ -15,6 +15,6 @@ namespace PlatinumGym.Core.Services
         Task<T> Create(T entity);
         Task<T> Update(T entity);
         Task<bool> Delete(int id);
-
+        Task<T> CheckIfExistByName(string name);
     }
 }
