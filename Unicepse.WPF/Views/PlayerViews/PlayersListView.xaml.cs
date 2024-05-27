31fc9f9bd354df44d5efc7fca0e4a18e99038763
yazanash <yazan.ash.doonaas@gmail.com>
2024-05-27 22:16:04 +0300

    change app settings

--- Unicepse.WPF/Views/PlayerViews/PlayersListView.xaml.cs
+++ Unicepse.WPF/Views/PlayerViews/PlayersListView.xaml.cs
@@ -31,48 +31,48 @@ namespace Unicepse.WPF.Views.PlayerViews
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    Microsoft.Office.Interop.Excel._Application application = new Microsoft.Office.Interop.Excel.Application();
-            //    Microsoft.Office.Interop.Excel._Workbook workbook = application.Workbooks.Add(Type.Missing);
-            //    Microsoft.Office.Interop.Excel._Worksheet? worksheet = null;
-            //    worksheet = workbook.Sheets["Sheet1"];
-            //    worksheet = workbook.ActiveSheet;
-            //    worksheet.Name = "player details";
-            //    worksheet.Cells.ColumnWidth = 25;
-            //    for (int i = 1; i < PlayerDataGrid.Columns.Count ; i++)
-            //    {
-            //        worksheet.Cells[1, i] = PlayerDataGrid.Columns[i - 1].Header.ToString();
+            try
+            {
+                Microsoft.Office.Interop.Excel._Application application = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel._Workbook workbook = application.Workbooks.Add(Type.Missing);
+                Microsoft.Office.Interop.Excel._Worksheet? worksheet = null;
+                worksheet = workbook.Sheets["Sheet1"];
+                worksheet = workbook.ActiveSheet;
+                worksheet.Name = "player details";
+                worksheet.Cells.ColumnWidth = 25;
+                for (int i = 1; i < PlayerDataGrid.Columns.Count ; i++)
+                {
+                    worksheet.Cells[1, i] = PlayerDataGrid.Columns[i - 1].Header.ToString();
 
-            //    }
-            //    for (int i = 0; i < PlayerDataGrid.Items.Count; i++)
-            //    {
-            //        var item = PlayerDataGrid.Items[i] as PlayerListItemViewModel;
-            //        worksheet.Cells[i + 2, 1] = item!.Id;
-            //        worksheet.Cells[i + 2, 2] = item!.FullName;
-            //        worksheet.Cells[i + 2, 3] = item!.BirthDate;
-            //        worksheet.Cells[i + 2, 4] = item!.Gendertext;
-            //        worksheet.Cells[i + 2, 5] = item!.SubscribeDate;
+                }
+                for (int i = 0; i < PlayerDataGrid.Items.Count; i++)
+                {
+                    var item = PlayerDataGrid.Items[i] as PlayerListItemViewModel;
+                    worksheet.Cells[i + 2, 1] = item!.Id;
+                    worksheet.Cells[i + 2, 2] = item!.FullName;
+                    worksheet.Cells[i + 2, 3] = item!.BirthDate;
+                    worksheet.Cells[i + 2, 4] = item!.Gendertext;
+                    worksheet.Cells[i + 2, 5] = item!.SubscribeDate;
 
-            //        worksheet.Cells[i + 2, 6] = item!.SubscribeEndDate;
-            //        worksheet.Cells[i + 2, 7] = item!.DayLeft;
-            //        worksheet.Cells[i + 2, 8] = item!.Balance;
-            //    }
-            //    var saveFileDialog = new SaveFileDialog();
-            //    saveFileDialog.FileName = "platinum";
-            //    saveFileDialog.DefaultExt = ".xlsx";
-            //    saveFileDialog.Filter = "Excel spreadsheet (*.xlsx)|*.xlsx";
-            //    if (saveFileDialog.ShowDialog() == true)
-            //    {
-            //        workbook.SaveAs(saveFileDialog.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing);
-            //    }
-            //    application.Quit();
-            //}
-            //catch
-            //{
-            //    MessageBox.Show("Excel not installed we will print it with CSV Format");
-            //      WriteCSVFile();
-            //}
+                    worksheet.Cells[i + 2, 6] = item!.SubscribeEndDate;
+                    worksheet.Cells[i + 2, 7] = item!.DayLeft;
+                    worksheet.Cells[i + 2, 8] = item!.Balance;
+                }
+                var saveFileDialog = new SaveFileDialog();
+                saveFileDialog.FileName = "platinum";
+                saveFileDialog.DefaultExt = ".xlsx";
+                saveFileDialog.Filter = "Excel spreadsheet (*.xlsx)|*.xlsx";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    workbook.SaveAs(saveFileDialog.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing);
+                }
+                application.Quit();
+            }
+            catch
+            {
+                MessageBox.Show("Excel not installed we will print it with CSV Format");
+                  WriteCSVFile();
+            }
 
           
         }
